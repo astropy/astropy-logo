d0c075c8af0732e4dce97d9405b037e0092e97f4
Thomas Robitaille <thomas.robitaille@gmail.com>
2012-10-18 21:15:00 +0200

    Added single word logo

--- convert.sh
+++ convert.sh
@@ -8,19 +8,23 @@ inkscape astropy_logo.svg --export-png=generated/astropy_logo.png
 inkscape astropy_logo_linkout.svg --export-png=generated/astropy_logo_linkout.png
 inkscape astropy_logo_docs.svg --export-png=generated/astropy_logo_docs.png
 inkscape astropy_logo_small.svg --export-png=generated/astropy_logo_small.png
+inkscape astropy_logo_word.svg --export-png=generated/astropy_logo_word.png
 
 # Convert form Inkscape SVG to Plain SVG (for web use)
 inkscape astropy_logo.svg --export-plain-svg=generated/astropy_logo_plain.svg
 inkscape astropy_logo_linkout.svg --export-plain-svg=generated/astropy_logo_linkout_plain.svg
 inkscape astropy_logo_docs.svg --export-plain-svg=generated/astropy_logo_docs_plain.svg
 inkscape astropy_logo_small.svg --export-plain-svg=generated/astropy_logo_small_plain.svg
+inkscape astropy_logo_word.svg --export-plain-svg=generated/astropy_logo_word_plain.svg
 
 # Convert to PNGs required by Astropy website/docs
 convert -intent Saturation -colorspace sRGB -filter Lanczos -resize 1000x96 -trim generated/astropy_logo.png generated/astropy_banner_96.png
 convert -colorspace sRGB -filter Box -resize 1000x31 -trim generated/astropy_logo_docs.png generated/astropy_docs_32.png
 convert -colorspace sRGB -filter Box -resize 1000x20 -trim generated/astropy_logo_linkout.png generated/astropy_linkout_20.png
+convert -colorspace sRGB -filter Box -resize 1000x32 -trim generated/astropy_logo_word.png generated/astropy_word_32.png
 
 # Remove full-res PNGs since these aren't needed
 rm generated/astropy_logo.png
 rm generated/astropy_logo_docs.png
 rm generated/astropy_logo_linkout.png
+rm generated/astropy_logo_word.png
